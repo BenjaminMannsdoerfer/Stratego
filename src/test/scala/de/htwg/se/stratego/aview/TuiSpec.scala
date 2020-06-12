1f8eadb6e509ffa68a81a5b1c2c54dcd7ccb9eba
WalterVoetschCortes <walter.vxd@googlemail.com>
2020-06-12 14:17:14 +0200

    testing

--- src/test/scala/de/htwg/se/stratego/aview/TuiSpec.scala
+++ src/test/scala/de/htwg/se/stratego/aview/TuiSpec.scala
@@ -8,7 +8,7 @@ import org.scalatest.{Matchers, WordSpec}
 class TuiSpec  extends WordSpec with Matchers{
 
   "A Stratego Tui" should {
-    val controller = new Controller(new MatchField(4,4,false))
+    val controller = new Controller(new MatchField(4, 4, false))
     val tui = new Tui(controller)
 
     "create and empty Matchfield on input 'n'" in {
@@ -23,24 +23,33 @@ class TuiSpec  extends WordSpec with Matchers{
     "move a Character downwards on input 'd 0 0'" in {
       controller.initMatchfield()
       tui.processInputLine("d 0 0")
-      controller.moveDown(1,0) should be ()
+      controller.moveDown(0, 1) should be()
     }
     "move a Character upwards on input 'u 3 0'" in {
       controller.initMatchfield()
       tui.processInputLine("u 3 0")
-      controller.moveUp(2,0) should be ()
+      controller.moveUp(3, 1) should be()
     }
-    "move a Character to the right on input 'r 2 0'" in {
+    "move a Character to the right" in {
       controller.initMatchfield()
-      tui.processInputLine("u 3 0")
-      tui.processInputLine("r 2 0")
-      controller.moveRight(2,1) should be ()
+      controller.moveRight(0, 0) should be()
     }
-    "move a Character to the left on input 'l 2 3'" in {
+    "move a Character to the left" in {
       controller.initMatchfield()
-      tui.processInputLine("u 3 3")
-      tui.processInputLine("l 2 3")
-      controller.moveLeft(2,2) should be ()
+      controller.moveLeft(0, 3) should be()
+    }
+    "attack a Characters on input 'a 0 0 0 1" in {
+      controller.initMatchfield()
+      tui.processInputLine("a 0 0 0 1")
+      controller.attack(0, 1, 0, 2) should be()
+    }
+    "set a Character of playerA on input ''" in {
+      tui.processInputLine("o 0 0 F")
+      controller.setBlueField(0, 1, "9")
+    }
+    "set a Character of playerB on input ''" in {
+      tui.processInputLine("t 0 0 F")
+      controller.setRedField(0, 1, "9")
     }
     "quit" in {
       tui.processInputLine("q")
--- src/test/scala/de/htwg/se/stratego/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/stratego/model/GameSpec.scala
@@ -10,7 +10,7 @@ class GameSpec extends WordSpec with Matchers {
       val playerRed = Player("PlayerRed", characList.getCharacterList())
       val game = Game(playerBlue, playerRed, 4, matchField)
       "fill the Matchfield with Characters" in {
-        game.init().toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  9  |  8  |  6  |  F  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  9  |  8  |  6  |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.init().toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  9  |  8  |  6  |  F  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  9  |  8  |  6  |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
     "figure set" should {
@@ -34,7 +34,7 @@ class GameSpec extends WordSpec with Matchers {
       val playerRed = Player("PlayerRed", characList.getCharacterList())
       val game = Game(playerBlue, playerRed, 4, matchField)
       "filled with characters" in {
-        game.aChar(matchField, 0, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  9  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.aChar(matchField, 0, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  9  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
     "gets a matchfield with a redList" should {
@@ -44,7 +44,7 @@ class GameSpec extends WordSpec with Matchers {
       val playerRed = Player("PlayerRed", characList.getCharacterList())
       val game = Game(playerBlue, playerRed, 4, matchField)
       "filled with characters" in {
-        game.bChar(matchField, 1, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  8  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.bChar(matchField, 1, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  8  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
 
@@ -54,9 +54,9 @@ class GameSpec extends WordSpec with Matchers {
       val playerBlue = Player("PlayerBlue", characList.getCharacterList())
       val playerRed = Player("PlayerRed", characList.getCharacterList())
       val game = Game(playerBlue, playerRed, 4, matchField)
-      val board = game.aChar(matchField, 0, 0, 0)
+      val board = game.init
       "and now one character moves down" in {
-        game.moveDown(board, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|  9  |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveDown(board, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |  8  |  6  |  F  | 0\n+-----+-----+-----+-----+\n|  9  |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  9  |  8  |  6  |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm")
       }
     }
 
@@ -68,7 +68,7 @@ class GameSpec extends WordSpec with Matchers {
       val game = Game(playerBlue, playerRed, 4, matchField)
       val board = game.aChar(matchField, 1, 3, 0)
       "and now one character moves up" in {
-        game.moveUp(board, 3, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|  8  |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveUp(board, 3, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|  8  |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
 
@@ -80,7 +80,7 @@ class GameSpec extends WordSpec with Matchers {
       val game = Game(playerBlue, playerRed, 4, matchField)
       val board = game.aChar(matchField, 2, 0, 1)
       "and now one character moves to the left" in {
-        game.moveLeft(board, 0, 1).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  6  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveLeft(board, 0, 1).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|  6  |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
     "gets a matchfield with a red and a blue List each with filled characters" should {
@@ -91,7 +91,7 @@ class GameSpec extends WordSpec with Matchers {
       val game = Game(playerBlue, playerRed, 4, matchField)
       val board = game.aChar(matchField, 3, 0, 0)
       "and now one character moves to the right" in {
-        game.moveRight(board, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |  F  |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveRight(board, 0, 0).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |  F  |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
 
@@ -103,7 +103,7 @@ class GameSpec extends WordSpec with Matchers {
       val game = Game(playerBlue, playerRed, 4, matchField)
       var board = game.aChar(matchField, 3, 3, 3)
       "and now one character moves to the right out of bounds" in {
-        game.moveRight(board, 3, 3).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveRight(board, 3, 3).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
     "gets a matchfield with a red and a blue List each with filled characters" should {
@@ -115,7 +115,7 @@ class GameSpec extends WordSpec with Matchers {
       val board = game.aChar(matchField, 2, 3, 2)
       val board1 = game.aChar(board, 3, 3, 3)
       "and now one character moves to the right out on another character" in {
-        game.moveRight(board1, 3, 2).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |  6  |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\nq:   quit the programm\n")
+        game.moveRight(board1, 3, 2).toString should be("   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |  6  |  F  | 3\n+-----+-----+-----+-----+\n**********  WELCOME TO STRATEGO  **********\n\nn:   create a new empty machtfield\ni:   set all character on the matchfield\nu:   move one character up\nd:   move one character down\nr:   move one character to the right\nl:   move one character to the left\na:   attack the character next to you\no:   player one can set his characters\nt:   player two can set his characters\nq:   quit the programm\n")
       }
     }
   }
