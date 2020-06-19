3d6d161a17abd3f1b63f43136123fa56866f1df3
WalterVoetschCortes <walter.vxd@googlemail.com>
2020-06-19 18:06:52 +0200

    gui 19.06

--- src/main/scala/de/htwg/se/stratego/aview/gui/SwingGui.scala
+++ src/main/scala/de/htwg/se/stratego/aview/gui/SwingGui.scala
@@ -1,35 +1,59 @@
 package de.htwg.se.stratego.aview.gui
 
+import java.awt.image.BufferedImage
+
 import scala.swing._
 import scala.swing.Swing.LineBorder
 import scala.swing.event._
 import de.htwg.se.stratego.controller._
 import de.htwg.se.stratego.controller.Controller
 import javax.imageio.ImageIO
-import javax.swing.ImageIcon
+import javax.swing.{ImageIcon, JOptionPane}
+
+class CellClicked(val row:Int, val column: Int) extends Event
 
 class SwingGui(controller:Controller) extends Frame{
   listenTo(controller)
 
-  title = "STRATEGO"
+  title = "Stratego"
   val matchFieldSize = controller.matchField.fields.matrixSize
 
 
-  val label =new Label{ icon= new ImageIcon("C:\\Users\\walte\\OneDrive\\Dokumente\\Sonstiges\\Stratego\\src\\main\\scala\\de\\htwg\\se\\stratego\\aview\\gui\\matchfield.JPG")}
+  //controller.initMatchfield()
 
-  iconImage = toolkit.getImage("C:\\Users\\walte\\OneDrive\\Dokumente\\Sonstiges\\Stratego\\src\\main\\scala\\de\\htwg\\se\\stratego\\aview\\gui\\matchfield.JPG")
+  def enterNamesDialog(){
+    Dialog.showMessage(null,"WELCOME TO STRATEGO!", title = "Stratego")
+    controller.setPlayers(JOptionPane.showInputDialog(null, "Spieler 1 Name:", JOptionPane.DEFAULT_OPTION) +
+    " " + JOptionPane.showInputDialog(null,"Spieler 2 Name:",title="Stratego"))
+    Dialog.showMessage(null, "Hello " + controller.playerList(0) + " and " + controller.playerList(1))
+  }
+  enterNamesDialog()
 
-  val img = toolkit.getImage("matchfield.JPG")//ImageIO.read("matchfield.JPG")
-
-  val panel = new Panel{
-    override def paint(g:Graphics2D): Unit ={
-      g.drawImage(img,100,100,null)
+  val matchfieldPanel = new GridPanel(matchFieldSize,matchFieldSize){
+    border = LineBorder(java.awt.Color.ORANGE, 2)
+    background = java.awt.Color.GRAY
+    for{
+      row <- 0 until matchFieldSize
+      col <- 0 until matchFieldSize
+    }{
+      contents += new FlowPanel(){
+        border = LineBorder(java.awt.Color.BLACK,2)
+        contents += new Label{
+          text = if(controller.matchField.fields.field(row, col).isSet) controller.matchField.fields.field(row,col).character.get.figure.name else " "
+          font = new Font("Verdana", 1, 36)
+        }
+      }
     }
-    preferredSize = new Dimension(500,500)
 
   }
+  /*
+    contents+= new Label {
+      icon = new ImageIcon("C:\\Users\\walte\\OneDrive\\Dokumente\\Sonstiges\\Stratego\\src\\main\\scala\\de\\htwg\\se\\stratego\\aview\\gui\\pikachu-figur.jpg")
+    }
+     */
 
-  menuBar = new MenuBar {
+
+    menuBar = new MenuBar {
     contents += new Menu("File") {
       mnemonic = Key.F
       contents += new MenuItem(Action("New") {
@@ -39,7 +63,17 @@ class SwingGui(controller:Controller) extends Frame{
         System.exit(0)
       })
     }
+      contents += new Menu("Edit"){
+        mnemonic = Key.E
+        contents+= new MenuItem(Action("Undo") { controller.undo })
+        contents += new MenuItem(Action("Redo") { controller.redo })
+      }
   }
+
+  matchfieldPanel.visible=true
+
+  contents_=(matchfieldPanel)
+
 
 
   visible = true
