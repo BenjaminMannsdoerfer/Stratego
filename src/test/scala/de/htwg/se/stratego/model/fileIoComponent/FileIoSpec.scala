e5b51ebeda18abc4c92230bcfb629d2761ac383a
Benjamin Mannsdörfer <b.mannsdoerfer@gmx.de>
2020-07-14 20:33:47 +0200

    fixed FileIoSpec

--- src/test/scala/de/htwg/se/stratego/model/fileIoComponent/FileIoSpec.scala
+++ src/test/scala/de/htwg/se/stratego/model/fileIoComponent/FileIoSpec.scala
@@ -13,23 +13,20 @@ class FileIoSpec extends WordSpec with Matchers {
       val playerBlue = Player("PlayerBlue", characList.getCharacterList())
       val playerRed = Player("PlayerRed", characList.getCharacterList())
       val game = Game(playerBlue, playerRed, 4, matchField)
+      val board = game.init(matchField)
       val controller = new Controller(matchField)
 
       "save and load the Gamestate with Json" in {
         import de.htwg.se.stratego.model.fileIoComponent.fileIoJsonImpl.FileIO
         val fileIOJson = new FileIO()
-        fileIOJson.save(matchField,controller.currentPlayerIndex)
-        fileIOJson.load.toString() should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|" +
-          "     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----" +
-          "+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |" +
-          "     |     | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create" +
-          " a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0)")
+        fileIOJson.save(board,controller.currentPlayerIndex)
+        fileIOJson.load.toString() should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|  \u001B[0;34m9\u001B[0m  |  \u001B[0;34m8\u001B[0m  |  \u001B[0;34m6\u001B[0m  |  \u001B[0;34mF\u001B[0m  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  \u001B[0;31m9\u001B[0m  |  \u001B[0;31m8\u001B[0m  |  \u001B[0;31m6\u001B[0m  |  \u001B[0;31mF\u001B[0m  | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0)")
       }
       "save and load the Gamestate with XML" in {
         import de.htwg.se.stratego.model.fileIoComponent.fileIoXmlImpl.FileIO
         val fileIOXML = new FileIO()
-        fileIOXML.save(matchField,controller.currentPlayerIndex)
-        fileIOXML.load.toString() should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|     |     |     |     | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|     |     |     |     | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0)")
+        fileIOXML.save(board,controller.currentPlayerIndex)
+        fileIOXML.load.toString() should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|  \u001B[0;34m9\u001B[0m  |  \u001B[0;34m8\u001B[0m  |  \u001B[0;34m6\u001B[0m  |  \u001B[0;34mF\u001B[0m  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  \u001B[0;31m9\u001B[0m  |  \u001B[0;31m8\u001B[0m  |  \u001B[0;31m6\u001B[0m  |  \u001B[0;31mF\u001B[0m  | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0)")
       }
     }
   }
